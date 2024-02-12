028396b49494e227ae87d12970d8f828c27bdad2
ARtiDPA <128789278+ARtiDPA@users.noreply.github.com>
2024-02-13 03:57:21 +0700

    Добавил папку под html шаблоны

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+@app.route("/add")
+def add_route():
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
